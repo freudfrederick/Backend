6a660963ddc7331bf7327fd50d58cb7016e267a4
freudfrederick <freud.frederick@iesb.edu.br>
2025-08-20 21:30:58 -0300

    Aluno e notas.

---
+++ praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,21 @@
+console.log("Rodando meu primeiro projeto em NODE.")
+
+let nome = "Freud"
+
+let sobreNome = "Frederick"
+
+console.log(nome + " " + sobreNome)
+
+let idade = 23
+
+console.log("A idade do " + nome + " é " + idade + " anos.")
+
+let exercicios = 0.5
+
+let trabalho = 1.5
+
+let prova = 4.5
+
+let n1 = exercicios + trabalho + prova
+
+console.log("NOTA A1: " + n1)
