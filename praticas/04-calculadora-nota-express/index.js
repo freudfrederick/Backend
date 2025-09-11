2e46ff23c6890872fe4d1d7a23d74824c04da194
freudfrederick <freud.frederick@iesb.edu.br>
2025-09-11 20:09:03 -0300

    Exercícios e práticas.

--- praticas/04-calculadora-nota-express/index.js
+++ praticas/04-calculadora-nota-express/index.js
@@ -6,22 +6,30 @@ const app = express()
 
 // Middlewares (Intermediários)
 // Intermediário de log
-app.use((req, rest, next) => {
-    console.log("------------####--------------")
+app.use((req, res, next) => {
+    console.log("------------####------------")
     console.log("Tempo: ", new Date().toLocaleString())
     console.log("Método: ", req.method)
-    console.log("Rota> ", req.url)
+    console.log("Rota: ", req.url)
+    next()
 })
 
+app.get('/nome', (req, res, next) => {
+    // Capturar informação do usuário através dos parametros da requisição
+    
+    const primeiroNome = req.query.primeiroNome
+    const sobreNome = req.query.sobreNome
 
+    res.send("Olá " + primeiroNome + " " + sobreNome + "!!!")
+})
 
+// Importando o router calculadora de nota
+const calculadoraNotaRouter = require('./routes/CalculadoraNota.js')
 
-
-
-
-
+//Toda requisição que chegar na rota /calculadora vai para router
+app.use('/calculadora', calculadoraNotaRouter)
 
 // executar a aplicação
 app.listen(3000, () => {
-    console.log("Aplicação rodando em http:localhost:3000")
+    console.log("Aplicação rodando em http://localhost:3000")
 })
---
+++ praticas/04-calculadora-nota-express/routes/CalculadoraNota.js
@@ -0,0 +1,30 @@
+// importar o express
+const express = require('express')
+
+//criar um router(Roteador)
+const router = express.Router()
+
+// Mapeamento das rotas e implemento à lógica
+
+router.get('/notaA1', (req, res, next) => {
+    const exercicio = parseFloat(req.query.exercicio)
+    const trabalho = parseFloat(req.query.trabalho)
+    const prova = parseFloat(req.query.prova)
+    
+    // validar se os parametros 
+
+    if(isNaN(exercicio) || isNaN(trabalho) || isNaN(prova)) {
+        return res.status(400).json({ erro: "Notas inválidas!!" })
+    }
+
+    // validar se as notas estão no intervalo correto
+    if(exercicio < 0 || exercicio > 1 || trabalho < 0 || trabalho > 3 || prova < 0 || prova > 6) {
+        return res.status(400).json({erro: "Notas fora do intervalo!!"})
+    }
+
+    const notaA1 = exercicio + trabalho + prova
+
+    res.json({notaA1})
+})
+
+module.exports = router
